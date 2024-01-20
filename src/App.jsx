f3b6ed3d6a1f607ab99db2d96ecc35f604a9cbf8
Braynner Marques <braynnermarques@hotmail.com>
2024-01-19 23:09:04 -0300

    adicionado as tags html

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,33 @@ function App() {
 
   return (
     <div className="container">
-      
+      <h2>Reliable, efficient delivery</h2>
+      <h2>Powered by Technology</h2>
+      <p>Our Artificial intelligence powered tools use millions of project data points to ensure that your project is successful</p>
+      <div className='cards'>
+        <div className='card-1'>
+          <h3>Supervisor</h3>
+          <p>Monitors activity to identify project roadblocks</p>
+          <img src="" alt="lupa" />
+        </div>
+        <div className='card-group'>
+          <div className='card-2'>
+            <h3>Team Builder</h3>
+            <p>Scans our talent network to create the optimal team for your project</p>
+            <img src="" alt="casa" />
+          </div>
+          <div className='card-3'>
+            <h3>Karma</h3>
+            <p>Regularly evaluates our talent to ensure quality</p>
+            <img src="" alt="lampada" />
+          </div>
+        </div>        
+        <div className='card-4'>
+          <h3>Calculator</h3>
+          <p>Uses data from past projects to provide better delivery estimates</p>
+          <img src="" alt="stats" />
+        </div>
+      </div>
     </div>
   )
 }
