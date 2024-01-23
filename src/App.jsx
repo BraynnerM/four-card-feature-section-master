a44ad1a64bcab4734ba01194d7f5c1c3fbf8669d
Braynner Marques <braynnermarques@hotmail.com>
2024-01-22 21:02:21 -0300

    acrescentados as imagens svg de cada card

--- src/App.jsx
+++ src/App.jsx
@@ -14,24 +14,24 @@ function App() {
         <div className='card-1'>
           <h3>Supervisor</h3>
           <p>Monitors activity to identify project roadblocks</p>
-          <img src="" alt="lupa" />
+          <img src="./images/icon-supervisor.svg" alt="lupa" />
         </div>
         <div className='card-group'>
           <div className='card-2'>
             <h3>Team Builder</h3>
             <p>Scans our talent network to create the optimal team for your project</p>
-            <img src="" alt="casa" />
+            <img src="./images/icon-team-builder.svg" alt="casa" />
           </div>
           <div className='card-3'>
             <h3>Karma</h3>
             <p>Regularly evaluates our talent to ensure quality</p>
-            <img src="" alt="lampada" />
+            <img src="./images/icon-karma.svg" alt="lampada" />
           </div>
         </div>        
         <div className='card-4'>
           <h3>Calculator</h3>
           <p>Uses data from past projects to provide better delivery estimates</p>
-          <img src="" alt="stats" />
+          <img src="./images/icon-calculator.svg" alt="stats" />
         </div>
       </div>
     </div>
