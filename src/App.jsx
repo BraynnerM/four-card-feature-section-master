153057597d2debd26caf0a90210f90c1dc757504
Braynner Marques <braynnermarques@hotmail.com>
2024-01-31 22:04:52 -0300

    versao mobile finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,11 @@ function App() {
           <img src="./images/icon-calculator.svg" alt="stats" />
         </div>
       </div>
-    </div>
+      <div className="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
+    Coded by <a href="https://www.linkedin.com/in/braynner/" target="_blank" rel="noreferrer">BraynnerM</a>.
+  </div>
+    </div>    
   );
 }
 
